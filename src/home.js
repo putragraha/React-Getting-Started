7691fb1548611e0d45b135132113dd450ee9babb
putragraha <putra.graha.7@gmail.com>
2021-12-13 22:46:25 +0700

    Click Events

--- src/home.js
+++ src/home.js
@@ -1,7 +1,14 @@
+const handleClick = (event) => console.log("Hello world", event)
+
+const handleClickWithArgument = (name, event) => console.log("Hello " + name, event.target)
+
 const Home = () => {
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <br />
+            <button onClick={handleClick}>Click</button>
+            <button onClick={(event) => handleClickWithArgument("Vention", event)}>Click with Argument</button>
         </div>
      );
 }
