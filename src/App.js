f9459fafa9928f5a79c641f6762d51bb43a689da
putragraha <putra.graha.7@gmail.com>
2021-12-13 22:01:28 +0700

    Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog"
+  const likes = 50
+  const link = "https://www.google.com"
+
   return (
     <div className="App">
       <div classsName="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ Math.random() * 10 }</p>
+        
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
